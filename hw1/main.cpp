d96d1d4545ab403c9433e23a4950b2ab27d73b4c
zhangjiaxun <20302010032@fudan.edu.cn>
2023-04-05 21:11:43 +0800

    update hw1

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -22,25 +22,48 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
 Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
-
+    using namespace std;
     // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
-
+    float angle_rad = rotation_angle/180.0f*acos(-1.0f);
+    printf("A");
+    model << cos(angle_rad), -sin(angle_rad), 0.0, 0.0,
+             sin(angle_rad),  cos(angle_rad), 0.0, 0.0,
+             0.0, 0.0, 1.0, 0.0,
+             0.0, 0.0, 0.0, 1.0; 
+    printf("B\n");
     return model;
 }
 
+//define fov means vertical fov. radio means width:height
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
                                       float zNear, float zFar)
 {
     // Students will implement this function
-
+    using namespace std;
     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
-
+    Eigen::Matrix4f perspective2ortho, orthoscale,orthotrans;
     // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
-
+    perspective2ortho << zNear, 0, 0, 0,
+                         0, zNear, 0, 0,
+                         0, 0, zNear+zFar, -zNear*zFar,
+                         0, 0, 1, 0;
+    //following variables need to consider look at which direction and where is the geometry
+    float zdepth = zNear - zFar;
+    float yheight = -zNear*tanf(eye_fov/2);
+    float xwidth = yheight*aspect_ratio;
+    orthoscale << 2/xwidth,0,0,0,
+                  0,2/yheight,0,0,
+                  0,0,2/zdepth,0,
+                  0,0,0,1;
+    orthotrans << 1,0,0,0,
+                  0,1,0,0,
+                  0,0,1,-zdepth/2,
+                  0,0,0,1;
+    projection = orthoscale*orthotrans*perspective2ortho;
     return projection;
 }
 
